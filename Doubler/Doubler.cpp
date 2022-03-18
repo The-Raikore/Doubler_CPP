db868a1074f1c200a0293c2479283cd2c98424b6
The-Raikore <The_Raikore@protonmail.com>
2022-03-17 18:05:37 -0700

    added comments to Doubler.cpp

--- Doubler/Doubler.cpp
+++ Doubler/Doubler.cpp
@@ -4,7 +4,7 @@ int main()
 {
 	int x{ };
 	std::cout << "Please enter an integer\n";
-	std::cin >> x;
-	int y = x * 2;
-	std::cout << x << " times two is " << y;
+	std::cin >> x; //assign user input to x
+	int y = x * 2; //double it
+	std::cout << x << " times two is " << y; //create human readable script
 }
